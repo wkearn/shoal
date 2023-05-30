2347a833b2cd43b7c6b4f034736687fc123b513c
William Kearney <wkearn@gmail.com>
2023-05-30 16:02:14 +0200

    Add order-sorted unification for types

--- src/types.rs
+++ src/types.rs
@@ -81,10 +81,12 @@ impl TypeScheme {
     /// panics if `other` is a QuantifiedType, which should never be reached.
     fn occurs_check(&self, other: &Self) -> bool {
         if let TypeScheme::PlainType(s @ Type::TypeVar(_, _)) = self {
-	    match other {
-		Self::PlainType(t) => s.occurs_check(t),
-		Self::QuantifiedType(_,_) => unreachable!("Quantified type should be instantiated before occurs check is called")
-	    }
+            match other {
+                Self::PlainType(t) => s.occurs_check(t),
+                Self::QuantifiedType(_, _) => unreachable!(
+                    "Quantified type should be instantiated before occurs check is called"
+                ),
+            }
         } else {
             false
         }
@@ -97,6 +99,169 @@ pub struct TypeEnv(HashMap<Box<str>, TypeScheme>);
 impl TypeEnv {
     pub fn new() -> Self {
         Self::default()
+    }
+}
+
+/// A type substitution maps type variables to type schemes
+pub struct TypeSubstitution {
+    fresh_vars: u32,
+    substitution: HashMap<Box<str>, Type>,
+    overloads: HashMap<Box<str>, Vec<Box<str>>>,
+}
+
+impl TypeSubstitution {
+    /// Generate a fresh type variable with no overloading constraints
+    fn genvar(&mut self) -> Type {
+        self.genvar_with_ops(None)
+    }
+
+    /// Generate a fresh type variable with a set of overloading constraints
+    fn genvar_with_ops<I>(&mut self, ops: I) -> Type
+    where
+        I: IntoIterator<Item = Box<str>>,
+    {
+        self.fresh_vars += 1;
+        let s = format!("?X{}", self.fresh_vars);
+
+        let ops_hs = ops.into_iter().collect();
+        Type::TypeVar(s.into(), ops_hs)
+    }
+
+    /// Apply the type substitution to a Type
+    fn get(&self, t: &Type) -> Type {
+        match t {
+            // Primitive types return the same type
+            Type::Boolean => Type::Boolean,
+            Type::Integer => Type::Integer,
+            Type::Float64 => Type::Float64,
+            Type::Function(arg, body) => {
+                let at = self.get(arg);
+                let ft = self.get(body);
+                Type::Function(Box::new(at), Box::new(ft))
+            }
+            Type::BinaryFunction(arg0, arg1, body) => {
+                let at0 = self.get(arg0);
+                let at1 = self.get(arg1);
+                let ft = self.get(body);
+                Type::BinaryFunction(Box::new(at0), Box::new(at1), Box::new(ft))
+            }
+            Type::Array(body) => Type::Array(Box::new(self.get(body))),
+            Type::TypeVar(s, _) => match self.substitution.get(s) {
+                Some(u @ Type::TypeVar(_, _)) => self.get(u),
+                Some(bound) => bound.clone(),
+                None => t.clone(),
+            },
+        }
+    }
+
+    fn is_valid_overloading(&self, xs: &HashSet<Box<str>>, t: &Type) -> bool {
+	match t {
+	    Type::TypeVar(_,_) => false,
+	    Type::Function(_,_) => xs.is_empty(),
+	    Type::BinaryFunction(_,_,_) => xs.is_empty(),
+	    Type::Array(_) => xs.is_empty(),
+	    Type::Boolean => {
+		for x in xs.iter() {
+		    match self.overloads.get(x) {
+			Some(ts) => {if !ts.contains(&"Boolean".into()) { return false; } }			
+			None => {return false}
+		    }
+		}
+		return true;
+	    }
+	    Type::Integer => {
+		for x in xs.iter() {
+		    match self.overloads.get(x) {
+			Some(ts) => {if !ts.contains(&"Integer".into()) { return false; } }			
+			None => {return false}
+		    }
+		}
+		return true;
+	    }
+	    Type::Float64 => {
+		for x in xs.iter() {
+		    match self.overloads.get(x) {
+			Some(ts) => {if !ts.contains(&"Integer".into()) { return false; } }			
+			None => {return false}
+		    }
+		}
+		return true;
+	    }
+	}
+    }
+    
+    fn cs(&mut self,xs: &HashSet<Box<str>>, right: &Type) -> Result<(),Error> {
+	match right {
+	    Type::TypeVar(alpha,ops) => {
+		let beta = self.genvar_with_ops(ops.union(xs).cloned());
+		self.substitution.insert(alpha.clone(),beta);
+		Ok(())
+	    }
+	    _ => {
+		if self.is_valid_overloading(xs,right) {
+		    Ok(())
+		} else {
+		    Err(Error::TypeError(format!(
+                        "Invalid overloading of {{{}}} with {right}",
+                        xs.iter().cloned().collect::<Vec<Box<str>>>().join(",")
+                    )))
+		}
+	    }
+	}
+    }
+
+    /// Unify the left and right types in the given substitution
+    fn unify(&mut self, left: &Type, right: &Type) -> Result<(), Error> {
+        match left {
+            Type::Boolean => match right {
+                Type::Boolean => Ok(()),
+                Type::TypeVar(_, _) => self.unify(right, left),
+                _ => Err(Error::TypeError(format!("{left} != {right}"))),
+            },
+            Type::Integer => match right {
+                Type::Integer => Ok(()),
+                Type::TypeVar(_, _) => self.unify(right, left),
+                _ => Err(Error::TypeError(format!("{left} != {right}"))),
+            },
+            Type::Float64 => match right {
+                Type::Float64 => Ok(()),
+                Type::TypeVar(_, _) => self.unify(right, left),
+                _ => Err(Error::TypeError(format!("{left} != {right}"))),
+            },
+            Type::Array(body0) => match right {
+                Type::TypeVar(_, _) => self.unify(right, left),
+                Type::Array(body1) => self.unify(body0, body1),
+                _ => Err(Error::TypeError(format!("{left} != {right}"))),
+            },
+            Type::Function(arg0, body0) => match right {
+                Type::TypeVar(_, _) => self.unify(right, left),
+                Type::Function(arg1, body1) => self
+                    .unify(arg0, arg1)
+                    .and_then(|_| self.unify(body0, body1)),
+                _ => Err(Error::TypeError(format!("{left} != {right}"))),
+            },
+            Type::BinaryFunction(arg00, arg01, body0) => match right {
+                Type::TypeVar(_, _) => self.unify(right, left),
+                Type::BinaryFunction(arg10, arg11, body1) => self
+                    .unify(arg00, arg10)
+                    .and_then(|_| self.unify(arg01, arg11))
+                    .and_then(|_| self.unify(body0, body1)),
+		_ => Err(Error::TypeError(format!("{left} != {right}"))),
+            },
+	    Type::TypeVar(x,ops) => {
+		if left == right {
+		    Ok(())
+		} else if left.occurs_check(right) {
+		    Err(Error::TypeError(format!("Recursive type found: {left} == {right}")))
+		} else {
+		    // Substitute the right variable to make sure that it is reduced as far as possible
+		    let new_right = self.get(right);
+		    self.cs(ops,&new_right)?;
+		    self.substitution.insert(x.clone(), new_right);
+		    Ok(())
+		}
+	    }
+        }
     }
 }
 
