cb2bd54df6edb4164e59e42686048acc9ba39bfe
William Kearney <wkearn@gmail.com>
2023-05-30 12:54:53 +0200

    Add a REPL and parse source code

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,25 @@ pub mod parser;
 use crate::error::Error;
 
 pub fn run(src: &str) -> Result<(), Error> {
+    let ex: parser::sexpr::SExpr = src.parse()?;
+    let ast = parser::parse(&ex)?;
+    println!("{ast:?}");
     Ok(())
+}
+
+pub fn repl() -> Result<(),Error> {
+    loop {
+        let mut source = String::new();
+        match std::io::stdin().read_line(&mut source) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("{}", e);
+                continue;
+            }
+        };
+
+	let ex: parser::sexpr::SExpr = source.parse()?;
+	let ast = parser::parse(&ex)?;
+	println!("{ast:?}")
+    }    
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ use shoal;
 
 #[derive(Parser, Debug)]
 struct Args {
-    path: std::path::PathBuf,
+    path: Option<std::path::PathBuf>,
 }
 
 fn main() -> Result<(), shoal::error::Error> {
     let args = Args::parse();
-    let source = std::fs::read_to_string(args.path)?;
-    shoal::run(&source)
+    match args.path {
+	Some(path) => {
+	    let source = std::fs::read_to_string(path)?;
+	    shoal::run(&source)
+	}
+	None => {
+	    shoal::repl()
+	}
+    }
 }
