7436f00e77579946e7c1591f0fbc481f37aae452
William Kearney <will@wskearney.com>
2023-06-06 09:06:57 +0200

    Add notes about the different compiler passes

--- src/compiler.rs
+++ src/compiler.rs
@@ -5,7 +5,13 @@ use crate::error::Error;
 use crate::parser::Program;
 
 pub fn compile(_: &Program) -> Result<Program, Error> {
-    // Type inference and annotation    
+    // Type inference and annotation
+
+    // Overloading resolution
+
+    // Alpha renaming
+
+    // A-normalization
     
     Err(Error::CompileError(
         "Compiler pass not yet implemented".into(),
