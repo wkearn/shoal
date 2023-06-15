63d0363a44b158220e32eee7ccf2248e7ee1c1a3
William Kearney <will@wskearney.com>
2023-06-15 20:49:41 +0200

    Autoformat

--- src/parser.rs
+++ src/parser.rs
@@ -76,8 +76,8 @@ impl<T> Program<T> {
     pub fn expression(&self) -> &Expr<T> {
         &self.expression
     }
-    pub fn into_components(self) -> (Vec<Statement<T>>,Expr<T>) {
-	(self.definitions, self.expression)
+    pub fn into_components(self) -> (Vec<Statement<T>>, Expr<T>) {
+        (self.definitions, self.expression)
     }
 }
 
